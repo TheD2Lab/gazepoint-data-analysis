f9efa4234e048129b7aa6f00d43d3cd42bfeb2d1
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2023-12-03 18:11:21 -0800

    fixed JSON reader

--- src/analysis/mainSimple.java
+++ src/analysis/mainSimple.java
@@ -10,12 +10,12 @@ public class mainSimple {
         JSONParser parse = new JSONParser();
         JSONObject config = (JSONObject)parse.parse(new FileReader("src\\analysis\\mainConfig.json"));
 
-        JSONObject output = (JSONObject)config.get("output");
-        String directory = (String)output.get("location");
-        String folderName = (String)output.get("name");
+        //JSONObject output = (JSONObject)config.get("output");
+        String directory = (String)config.get("location");
+        String folderName = (String)config.get("name");
 
         System.out.println("directory: "+directory);
         System.out.println("folderName: "+folderName);
 
     }
-}
+} 
